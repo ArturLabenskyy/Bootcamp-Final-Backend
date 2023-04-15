0016539f4e448d865ab3c3994efb9dc3eaf2f114
ArturLabenskyy <dodik.dod@hotmail.com>
2023-04-15 21:56:15 +0300

    comments route fix

--- routes/commentRoute.js
+++ routes/commentRoute.js
@@ -4,16 +4,19 @@ import {
     getComment,
     updateComment,
     deleteComment,
+    getCommentsByPost,
 } from "../controllers/commentController.js";
 import { protect } from "../middleware/authMiddleware.js";
+import asyncHandler from "../middleware/asyncHandler.js";
 
 const router = express.Router();
 
 router.route("/").post(protect, createComment);
 router
     .route("/:commentId")
-    .get(protect, getComment)
-    .put(protect, updateComment)
-    .delete(protect, deleteComment);
+    .get(protect, asyncHandler(getComment))
+    .put(protect, asyncHandler(updateComment))
+    .delete(protect, asyncHandler(deleteComment));
+router.route("/:postId").get(protect, asyncHandler(getCommentsByPost));
 
 export default router;
